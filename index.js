84e39e47e09472a7a685ef3b8207af85eb45b00a
amusimg <3133145320@qq.com>
2022-11-03 00:31:59 +0800

    master分支修改了

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-<script>
-  console.log('合并分支');
 
-  console.log('master 分支修改了文件');
-</script>
+console.log('合并分支');
+
+console.log('master 分支修改了文件');
+
+(function (params) {
+  console.log(params);
+})('你是我的谁')
